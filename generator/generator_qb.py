f029223fb749782c2bd396c131e8706820a3124a
dima <dd@fivegen.ru>
2025-11-21 19:55:05 +0300

    tests

--- generator/generator_qb.py
+++ generator/generator_qb.py
@@ -1,31 +1,26 @@
-from pyqpanda3.core import QCircuit, QProg, H, measure, CPUQVM
+from math import sqrt
 
-# Функция для генерации случайного 64-битного токена с помощью квантового симулятора
-def generate_quantum_random_token():
-    qvm = CPUQVM()
-    circuit = QCircuit()
-    for i in range(2):
-        circuit << H(i)   # Применяем H-гейт к кубиту i (равные вероятности 0 и 1)
-        print(circuit)
-
-    prog = QProg()
-    prog << circuit
-
-    # Выполняем измерение каждого кубита в базисе Z, результаты сохраняем в классический бит с тем же индексом:contentReference[oaicite:3]{index=3}
-    for i in range(2):
-        prog << measure(i, i)  # Измеряем кубит i и сохраняем бит в память i
-
-    # Инициализируем QPanda3 симулятор на CPU и выполняем программу один раз (single shot):contentReference[oaicite:4]{index=4}
-
-    qvm.run(prog, 5)  # Запускаем программу на 1 повтор (количество шотов = 1)
-
-    # Получаем результаты: словарь, где ключ — 64-битная строка результатов измерений, значение — число выпадений
-    result = qvm.result().get_counts()
-
-    # Извлекаем единственную 64-битную строку (токен) из результатов
-    bitstring = list(result.keys())[0]
-    return bitstring
+from pyqpanda3.core import QCircuit, QProg, H, measure, CPUQVM, draw_qprog, I, CNOT
 
 
-token = generate_quantum_random_token()
-print("Сгенерированный 64-битный токен:", token)
+def generate_quantum():
+    prog = QProg(1)
+    prog << H(0)
+    prog << measure(0, 0)
+    prog << CNOT(0,1)
+    prog << measure(1, 1)
+    return prog
+
+
+print(draw_qprog(generate_quantum()))
+
+qvm = CPUQVM()
+qvm.run(generate_quantum(), 1024)
+rez = qvm.result().get_prob_dict()
+print(rez)
+print(f'{sqrt(rez["0"])} ∣0⟩ + {sqrt(rez["1"])} ∣1⟩')
+assert rez["0"] + rez["1"] == 1
+
+
+
+
