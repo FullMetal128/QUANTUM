7534a9c64a8d8b5cca0729b5d1cc1231ad2232f6
dima <dd@fivegen.ru>
2025-11-23 13:45:42 +0300

    begins

--- main/main.py
+++ main/main.py
@@ -1,15 +1,18 @@
 import numpy as np
 from qubits.qubit_func import *
 simulator = CPUQVM()
+number_of_qubits = 2
+shots = 10000
+
 #Создаем приватные кубиты
-privateQbitsArray = generate_random_private_qbits(number_of_qbits_in_token=3)
+privateQbitsArray = generate_random_private_qbits(number_of_qbits_in_token=number_of_qubits)
 print("Приватные кубиты:")
 for el in privateQbitsArray:
-    print("___________")
+    print("*********************")
     print("Кубит №", str(el.id))
     print("  Тета:", str(el.theta))
     print("  Фи:", str(el.phi))
     print("  Id дочернего публичного кубита:", str(el.publicQbit.id))
-    print(measure_qbit(simulator, el, number_of_measures_of_single_qbit=100))
-    print("___________")
+    print(measure_qbit(simulator, el, number_of_measures_of_single_qbit=shots))
+    print("*********************")
 print("")
