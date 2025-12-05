3b8de576586e8953f4e09162fab7f2c5d1ab84e5
dima <dd@fivegen.ru>
2025-12-05 20:29:21 +0300

    каркас построен

--- main/main.py
+++ main/main.py
@@ -1,9 +1,12 @@
+import time
+
 import numpy as np
 from qubits.qubit_func import *
 simulator = CPUQVM()
 number_of_qubits = 2
 shots = 10000
 numberOfMeasuresOfSingleQbit = 10000
+permissibleNumberOfones = 50
 
 #Создаем приватные кубиты
 privateQbitsArray = generate_random_private_qbits(number_of_qbits_in_token=number_of_qubits)
@@ -13,17 +16,50 @@ for el in privateQbitsArray:
     print("Кубит №", str(el.id))
     print("  Тета:", str(el.theta))
     print("  Фи:", str(el.phi))
-    print("  Id дочернего публичного кубита:", str(el.publicQbit.id))
+    print("  Id дочернего публичного кубита:", str(el.public_qbit.id))
     print(measure_qbit(simulator, el, number_of_measures_of_single_qbit=shots))
     print("*********************")
 print("")
 
 #Создаем массив публичных кубитов на основе приватных
-publicQbitsArray = make_public_qbits_array(privateQbitsArray)
+publicQbitsArray = make_public_qbits_array(private_qbits_array=privateQbitsArray)
 print("\nПубличные кубиты:")
 for el in publicQbitsArray:
+    print("___________")
+    print("Кубит №", str(el.id))
+
+    print(measureQbit(simulator, el, numberOfMeasuresOfSingleQbit))
+    print("___________")
+print("")
+
+#Создаем токен на основе публичных кубитов
+token = Token(1, publicQbitsArray)
+
+#Делаем спины на всех кубитах
+make_spin_for_all_qbits_in_token(token, privateQbitsArray)
+print("\n")
+
+print("Публичные кубиты внутри токена после спина:")
+for el in token.array_of_public_qbits:
     print("___________")
     print("Кубит №", str(el.id))
     print(measureQbit(simulator, el, numberOfMeasuresOfSingleQbit))
     print("___________")
 print("")
+
+time.sleep(1)
+
+#Разворачиваем все кубиты в токене
+reverse_qbits_in_token(token, privateQbitsArray)
+
+print("Публичные кубиты внутри токена после обратного спина:")
+for el in token.array_of_public_qbits:
+    print("___________")
+    print("Кубит №", str(el.id))
+    print(measureQbit(simulator, el, numberOfMeasuresOfSingleQbit))
+    print("___________")
+print("")
+
+#Замеряем все кубиты в нашем токене(для наглядности)
+resultOfTokenMeasure = measure_token(simulator, token, numberOfMeasuresOfSingleQbit, permissibleNumberOfones)
+print("Результат проверки: "+str(resultOfTokenMeasure))
--- qubits/qubit_func.py
+++ qubits/qubit_func.py
@@ -11,7 +11,7 @@ class PrivateQbit:
         self.phi = phi
         self.tag = tag
         self.circuit = QCircuit()
-        self.publicQbit = PublicQbit(self.id)
+        self.public_qbit = PublicQbit(self.id)
 
 #Класс, содержащий только суперпозицию. Набор объектов данного класса будут составлять токен
 class PublicQbit:
@@ -20,12 +20,13 @@ class PublicQbit:
         self.tag = tag
         self.circuit = QCircuit()
 
-    def makeSpin(self, theta, phi):
+    def make_spin(self, theta, phi):
         self.circuit << RY(self.id, math.radians(theta)) # θ влияет на P(0)/P(1) в Z
         self.circuit << RZ(self.id, math.radians(phi))   # φ задаёт фазу, в Z не видно
+        print(theta, phi)
         print("Выполнен спин на публичном кубите с id: "+str(self.id))
 
-    def makeReverseSpin(self, theta, phi):
+    def make_reverse_spin(self, theta, phi):
         self.circuit << RZ(self.id, math.radians(-phi))   # φ задаёт фазу, в Z не видно
         self.circuit << RY(self.id, math.radians(-theta)) # θ влияет на P(0)/P(1) в Z
         print("Выполнен обратный спин на публичном кубите с id: "+str(self.id))
@@ -34,8 +35,8 @@ class PublicQbit:
 def generate_random_private_qbits(number_of_qbits_in_token):
     resultArray = []
     for i in range(number_of_qbits_in_token):
-        privateQbit = PrivateQbit(i+1, get_random_theta(), get_random_phi())
-        resultArray.append(privateQbit)
+        private_qbit = PrivateQbit(i+1, get_random_theta(), get_random_phi())
+        resultArray.append(private_qbit)
     return resultArray
 
 #Функции для генерации случайных углов тета и фи
@@ -57,19 +58,67 @@ def measure_qbit(simulator, qbit, number_of_measures_of_single_qbit):
     return result
 
 #Создание массива публичных кубитов на основе приватных
-def make_public_qbits_array(private_qbits_array):
+def make_public_qbits_array(private_qbits_array: list):
     public_qbits_array = []
     for privateQbit in private_qbits_array:
-        public_qbits_array.append(privateQbit.publicQbit)
+        public_qbits_array.append(privateQbit.public_qbit)
     return public_qbits_array
 
 #Функция для измерения состояния кубита
-def measureQbit(simulator, qbit, numberOfMeasuresOfSingleQbit):
+def measureQbit(simulator, qbit, number_of_measures_of_single_qbit):
     programm = QProg() << qbit.circuit << measure(qbit.id, 0)    #qbit.id)
-    simulator.run(programm, numberOfMeasuresOfSingleQbit)
+    simulator.run(programm, number_of_measures_of_single_qbit)
     result = simulator.result().get_counts()
-    #print("Кубит с id "+str(qbit.id))
-    #print(result)
+    print("Кубит с id "+str(qbit.id))
     ones = result.get('1', 0)
-    #print("Количество единиц:", ones)
+    print("Количество единиц:", ones)
     return result
+
+#Функция для поиска приватного кубита в массиве по его айди(используется чтобы публичный кубит нашел свой приватный во время проверки токена)
+def find_private_qbit_by_id(private_qbits, id):
+    for el in private_qbits:
+        if el.id == id:
+            return el
+    print("Кубит с таким айди не найден")
+    return None
+
+#Сделать спин/задать суперпозицию на всех кубитах в токене
+def make_spin_for_all_qbits_in_token(token, private_qbits):
+    for public_qbit in token.array_of_public_qbits:
+        private_qbit = find_private_qbit_by_id(private_qbits, public_qbit.id)
+        public_qbit.make_spin(private_qbit.theta, private_qbit.phi)
+
+#Функция обратного спина для проверки токена
+def reverse_qbits_in_token(token, private_qbits):
+    for publicQbit in token.array_of_public_qbits:
+        private_qbit = find_private_qbit_by_id(private_qbits, publicQbit.id)
+        publicQbit.make_reverse_spin(private_qbit.theta, private_qbit.phi)
+
+#Функция для измерения состояний кубитов токена
+def measure_token(simulator, token, number_of_measures_of_single_qbit, permissible_number_of_ones):
+    if((datetime.now() - token.time_of_creation).total_seconds() > token.ttl):
+        return False
+    success = True
+    for qbit in token.array_of_public_qbits:
+        programm = QProg() << qbit.circuit << measure(qbit.id, 0)    #qbit.id)
+        simulator.run(programm, number_of_measures_of_single_qbit)
+        result = simulator.result().get_counts()
+        #print("Кубит с id "+str(qbit.id))
+        #print(result)
+        ones = result.get('1', 0)
+        #print("Количество единиц:", ones)
+        if ones > permissible_number_of_ones:
+            success = False
+    return success
+
+#Класс, описывающий токен
+class Token:
+    def __init__(self, id, array_of_public_qbits, tag=None):
+        self.time_of_creation = datetime.now()
+        self.ttl = 5
+        self.id = id
+        self.array_of_public_qbits = array_of_public_qbits
+        self.tag = tag
+
+    def add_qbit(self, public_qbit):
+        self.array_of_public_qbits.append(public_qbit)
